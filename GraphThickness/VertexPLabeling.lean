2a84898987dd010d8a46eeaa908a5622fe87c560
gloges <gerg1995@gmail.com>
2026-09-02 17:41:39 -0400

    minor changes

--- GraphThickness/VertexPLabeling.lean
+++ GraphThickness/VertexPLabeling.lean
@@ -13,9 +13,11 @@ public import Mathlib.Data.PFun
 -/
 @[expose] public section
 
+universe u v
+
 /-- A **partial vertex labeling** `VertexPLabeling V K` (or `V →ᵥ. K`)
   is a map which assigns type `K` labels to a subset of the "vertices" of type `V`. -/
-def VertexPLabeling (V K : Type*) := V → Option K
+def VertexPLabeling (V : Type u) (K : Type v) := V → Option K
 
 @[inherit_doc]
 infixr:25 " →ᵥ. " => VertexPLabeling
@@ -23,14 +25,22 @@ infixr:25 " →ᵥ. " => VertexPLabeling
 --~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
 namespace VertexPLabeling
 
-variable {V K : Type*}
+open Option Set
+
+variable {V : Type u} {K : Type v}
+
+@[ext]
+lemma ext {l l' : V →ᵥ. K} (h : ∀ v, l v = l' v) : l = l' := by
+  funext v
+  exact h v
 
 --~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
-section VertexSet
+section VertexSets
 
-variable (l : V →ᵥ. K) (k : K)
+variable (k : K) (l : V →ᵥ. K)
 
 /-- The set of vertices which are labeled by `l`. -/
+def labeled : Set V := {v | l v ≠ none}
 
 /-- The set of vertices which are not labeled by `l`. -/
 def unlabeled : Set V := {v | l v = none}
@@ -41,13 +51,13 @@ def vertexSet : Set V := {v | l v = some k}
 variable {l k}
 
 @[simp]
-lemma mem_labeled_iff {v : V} : v ∈ l.labeled ↔ l v ≠ none := Option.ne_none_iff_exists'.symm
+lemma mem_labeled_iff {v : V} : v ∈ l.labeled ↔ l v ≠ none := mem_ofPred
 
 @[simp]
-lemma mem_unlabeled_iff {v : V} : v ∈ l.unlabeled ↔ l v = none := Set.mem_ofPred
+lemma mem_unlabeled_iff {v : V} : v ∈ l.unlabeled ↔ l v = none := mem_ofPred
 
 @[simp]
-lemma mem_vertexSet_iff {v : V} : v ∈ l.vertexSet k ↔ l v = some k := Set.mem_ofPred
+lemma mem_vertexSet_iff {v : V} : v ∈ l.vertexSet k ↔ l v = some k := mem_ofPred
 
 variable (l k)
 
@@ -63,9 +73,9 @@ lemma unlabeled_compl : l.unlabeledᶜ = l.labeled := by
 
 lemma isCompl_labeled_unlabeled : IsCompl l.labeled l.unlabeled := l.labeled_compl ▸ isCompl_compl
 
-lemma vertexSet_subset : l.vertexSet k ⊆ l.labeled := fun _ h ↦ ⟨k, h⟩
+lemma vertexSet_subset : l.vertexSet k ⊆ l.labeled := fun _ h ↦ ne_none_iff_exists'.mpr ⟨k, h⟩
 
-end VertexSet
+end VertexSets
 --~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
 
 --~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
@@ -104,9 +114,7 @@ def update : V →ᵥ. K := fun v ↦ if l v = some k then some k' else l v
 lemma update_apply (v : V) : l.update k k' v = if l v = some k then some k' else l v := rfl
 
 @[simp]
-lemma update_same : l.update k k = l := by
-  funext
-  simp [Eq.comm]
+lemma update_same : l.update k k = l := by aesop
 
 @[simp]
 lemma update_labeled : (l.update k k').labeled = l.labeled := by aesop
