e2fc0126d455d08f17d556612356243c3abff4a0
Gregory Loges <gerg1995@gmail.com>
2026-09-03 15:45:02 -0400

    feat: add vertex pairing (#4)
    * reorganize (un)labeled
    
    * add pairing
    
    * split into pairing_inl/inr

--- GraphThickness/VertexPLabeling.lean
+++ GraphThickness/VertexPLabeling.lean
@@ -5,6 +5,7 @@ Authors: Gregory J. Loges
 -/
 module
 
+public import Mathlib.Data.Set.Restrict
 public import Mathlib.Order.BooleanAlgebra.Set
 /-!
 
@@ -39,11 +40,11 @@ section VertexSets
 
 variable (k : K) (l : V →ᵥ. K)
 
-/-- The set of vertices which are labeled by `l`. -/
-def labeled : Set V := {v | l v ≠ none}
-
 /-- The set of vertices which are not labeled by `l`. -/
 def unlabeled : Set V := {v | l v = none}
+
+/-- The set of vertices which are labeled by `l`. -/
+def labeled : Set V := {v | l v ≠ none}
 
 /-- The set of vertices which are assigned the label `k` by `l`. -/
 def vertexSet : Set V := {v | l v = some k}
@@ -51,10 +52,10 @@ def vertexSet : Set V := {v | l v = some k}
 variable {k l}
 
 @[simp]
-lemma mem_labeled_iff {v : V} : v ∈ l.labeled ↔ l v ≠ none := mem_ofPred
+lemma mem_unlabeled_iff {v : V} : v ∈ l.unlabeled ↔ l v = none := mem_ofPred
 
 @[simp]
-lemma mem_unlabeled_iff {v : V} : v ∈ l.unlabeled ↔ l v = none := mem_ofPred
+lemma mem_labeled_iff {v : V} : v ∈ l.labeled ↔ l v ≠ none := mem_ofPred
 
 @[simp]
 lemma mem_vertexSet_iff {v : V} : v ∈ l.vertexSet k ↔ l v = some k := mem_ofPred
@@ -62,16 +63,12 @@ lemma mem_vertexSet_iff {v : V} : v ∈ l.vertexSet k ↔ l v = some k := mem_of
 variable (k l)
 
 @[simp]
-lemma labeled_compl : l.labeledᶜ = l.unlabeled := by
-  ext
-  simp
+lemma unlabeled_compl : l.unlabeledᶜ = l.labeled := by aesop
 
 @[simp]
-lemma unlabeled_compl : l.unlabeledᶜ = l.labeled := by
-  ext
-  simp
+lemma labeled_compl : l.labeledᶜ = l.unlabeled := by aesop
 
-lemma isCompl_labeled_unlabeled : IsCompl l.labeled l.unlabeled := l.labeled_compl ▸ isCompl_compl
+lemma isCompl_unlabeled_labeled : IsCompl l.unlabeled l.labeled := l.unlabeled_compl ▸ isCompl_compl
 
 lemma vertexSet_subset : l.vertexSet k ⊆ l.labeled := fun _ h ↦ ne_none_iff_exists'.mpr ⟨k, h⟩
 
@@ -90,14 +87,14 @@ def remove : V →ᵥ. K := fun v ↦ if l v = some k then none else l v
 lemma remove_apply (v : V) : l.remove k v = if l v = some k then none else l v := rfl
 
 @[simp]
-lemma remove_labeled : (l.remove k).labeled = l.labeled \ l.vertexSet k := by
-  ext
-  simp [And.comm]
-
-@[simp]
 lemma remove_unlabeled : (l.remove k).unlabeled = l.unlabeled ∪ l.vertexSet k := by
   ext
   simp [or_iff_not_imp_right]
+
+@[simp]
+lemma remove_labeled : (l.remove k).labeled = l.labeled \ l.vertexSet k := by
+  ext
+  simp [And.comm]
 
 end Remove
 --~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
@@ -117,10 +114,10 @@ lemma update_apply (v : V) : l.update k k' v = if l v = some k then some k' else
 lemma update_same : l.update k k = l := by aesop
 
 @[simp]
-lemma update_labeled : (l.update k k').labeled = l.labeled := by aesop
+lemma update_unlabeled : (l.update k k').unlabeled = l.unlabeled := by aesop
 
 @[simp]
-lemma update_unlabeled : (l.update k k').unlabeled = l.unlabeled := by aesop
+lemma update_labeled : (l.update k k').labeled = l.labeled := by aesop
 
 end Update
 --~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
@@ -143,10 +140,10 @@ lemma sum_inl (v : V) : (l ⊕g l') (.inl v) = l v := rfl
 lemma sum_inr (w : W) : (l ⊕g l') (.inr w) = l' w := rfl
 
 @[simp]
-lemma sum_labeled : (l ⊕g l').labeled = .inl '' l.labeled ∪ .inr '' l'.labeled := by aesop
+lemma sum_unlabeled : (l ⊕g l').unlabeled = .inl '' l.unlabeled ∪ .inr '' l'.unlabeled := by aesop
 
 @[simp]
-lemma sum_unlabeled : (l ⊕g l').unlabeled = .inl '' l.unlabeled ∪ .inr '' l'.unlabeled := by aesop
+lemma sum_labeled : (l ⊕g l').labeled = .inl '' l.labeled ∪ .inr '' l'.labeled := by aesop
 
 @[simp]
 lemma sum_vertexSet (k : K) :
@@ -178,17 +175,17 @@ instance : PartialOrder (V →ᵥ. K) where
   le_trans _ _ _ _ _ _ := by aesop
   le_antisymm _ _ _ _ := by aesop
 
-lemma labeled_subset_of_le (h : l₁ ≤ l₂) : l₁.labeled ⊆ l₂.labeled := fun _ ↦ by aesop
-
 lemma unlabeled_subset_of_ge (h : l₁ ≤ l₂) : l₂.unlabeled ⊆ l₁.unlabeled := fun _ ↦ by aesop
+
+lemma labeled_subset_of_le (h : l₁ ≤ l₂) : l₁.labeled ⊆ l₂.labeled := fun _ ↦ by aesop
 
 lemma vertexSet_subset_of_le (k : K) (h : l₁ ≤ l₂) : l₁.vertexSet k ⊆ l₂.vertexSet k :=
   fun _ ↦ by aesop
 
-lemma labeled_monotone : Monotone (labeled : (V →ᵥ. K) → Set V) := fun _ _ ↦ labeled_subset_of_le
-
 lemma unlabeled_antitone : Antitone (unlabeled : (V →ᵥ. K) → Set V) :=
   fun _ _ ↦ unlabeled_subset_of_ge
+
+lemma labeled_monotone : Monotone (labeled : (V →ᵥ. K) → Set V) := fun _ _ ↦ labeled_subset_of_le
 
 lemma vertexSet_monotone (k : K) : Monotone (vertexSet k : (V →ᵥ. K) → Set V) :=
   fun _ _ ↦ vertexSet_subset_of_le k
@@ -214,10 +211,10 @@ instance : OrderBot (V →ᵥ. K) where
 lemma bot_apply (v : V) : (⊥ : V →ᵥ. K) v = none := rfl
 
 @[simp]
-lemma bot_labeled : (⊥ : V →ᵥ. K).labeled = ∅ := by aesop
+lemma bot_unlabeled : (⊥ : V →ᵥ. K).unlabeled = univ := by aesop
 
 @[simp]
-lemma bot_unlabeled : (⊥ : V →ᵥ. K).unlabeled = univ := by aesop
+lemma bot_labeled : (⊥ : V →ᵥ. K).labeled = ∅ := by aesop
 
 @[simp]
 lemma bot_vertexSet (k : K) : (⊥ : V →ᵥ. K).vertexSet k = ∅ := by aesop
@@ -232,6 +229,44 @@ lemma bot_update [DecidableEq K] (k k' : K) : (⊥ : V →ᵥ. K).update k k' = 
 lemma bot_sum_bot : (⊥ : V →ᵥ. K) ⊕g (⊥ : W →ᵥ. K) = ⊥ := by aesop
 
 end OrderBot
+--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
+
+--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
+section Pairing
+
+variable (l : V →ᵥ. K)
+
+/-- The map which is `id` on unlabeled vertices and `swap` on labeled vertices. -/
+def pairing : V ⊕ V → V ⊕ V :=
+  fun x =>
+    match (l ⊕g l) x with
+    | none => x
+    | some _ => x.swap
+
+@[simp]
+lemma pairing_inl (v : V) : l.pairing (.inl v) = if l v = none then .inl v else .inr v := by
+  dsimp [pairing]
+  aesop
+
+@[simp]
+lemma pairing_inr (v : V) : l.pairing (.inr v) = if l v = none then .inr v else .inl v := by
+  dsimp [pairing]
+  aesop
+
+@[simp]
+lemma pairing_apply_apply (x : V ⊕ V) : l.pairing (l.pairing x) = x := by aesop
+
+lemma pairing_involutive : Function.Involutive l.pairing := l.pairing_apply_apply
+
+/-- `l.pairing` acts as the identity on unlabeled vertices. -/
+lemma unlabeled_domRestrict_pairing :
+    (l ⊕g l).unlabeled.domRestrict l.pairing = Subtype.val := by aesop
+
+/-- `l.pairing` acts as `Sum.swap` on labeled vertices. -/
+lemma labeled_domRestrict_pairing :
+    (l ⊕g l).labeled.domRestrict l.pairing = Sum.swap ∘ Subtype.val := by aesop
+
+end Pairing
 --~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
 
 end VertexPLabeling
