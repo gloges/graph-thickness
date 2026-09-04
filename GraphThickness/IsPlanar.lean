7a9be9177200d66f3af42af7f906625b5f1c54ec
gloges <gerg1995@gmail.com>
2026-09-04 00:03:19 -0400

    add maps

--- GraphThickness/IsPlanar.lean
+++ GraphThickness/IsPlanar.lean
@@ -34,5 +34,36 @@ def IsPlanar (G : SimpleGraph V) : Prop :=
   ∃ f : V ↪ ℝ², ∀ v v' w w' : V, G.Adj v v' → G.Adj w w' →
     v ≠ w → v ≠ w' → v' ≠ w → v' ≠ w' → Disjoint [f v -[ℝ] f v'] [f w -[ℝ] f w']
 
+--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
+namespace IsPlanar
+
+--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
+section Maps
+
+/-- A graph that embeds into a planar graph is planar. -/
+lemma embedding {G : SimpleGraph V} {H : SimpleGraph W} (f : H ↪g G) (hG : G.IsPlanar) :
+    H.IsPlanar := by
+  obtain ⟨g, hg⟩ := hG
+  exact ⟨f.toEmbedding.trans g, fun v v' w w' ↦ by simpa using hg (f v) (f v') (f w) (f w')⟩
+
+/-- Planarity is preserved under graph isomorphism. -/
+lemma iso {G : SimpleGraph V} {H : SimpleGraph W} (f : H ≃g G) (hG : G.IsPlanar) : H.IsPlanar :=
+  hG.embedding f.toEmbedding
+
+/-- An induced subgraph of a planar graph is planar. -/
+lemma induce {G : SimpleGraph V} (s : Set V) (hG : G.IsPlanar) : (G.induce s).IsPlanar :=
+  hG.embedding <| .induce s
+
+/-- A subgraph of a planar simple graph is planar. -/
+lemma mono {G H : SimpleGraph V} (h : H ≤ G) (hG : G.IsPlanar) : H.IsPlanar := by
+  obtain ⟨g, hg⟩ := hG
+  exact ⟨g, fun v v' w w' hv hw ↦ hg v v' w w' (h hv) (h hw)⟩
+
+end Maps
+--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
+
+end IsPlanar
+--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
+
 end SimpleGraph
 --~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
