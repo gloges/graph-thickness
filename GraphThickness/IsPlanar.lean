cec40e5b740c42e7c76257ca7a1434355cff2379
Gregory Loges <gerg1995@gmail.com>
2026-09-08 13:16:51 -0400

    feat: add planar inequalities (#12)
    * injective hom
    
    * add toc
    
    * edge/vertex inequalities
    
    * empty graph planar
    
    * prove K5 non-planar

--- GraphThickness/IsPlanar.lean
+++ GraphThickness/IsPlanar.lean
@@ -6,10 +6,16 @@ Authors: Gregory J. Loges
 module
 
 public import Mathlib.Analysis.InnerProductSpace.PiL2
-public import Mathlib.Combinatorics.SimpleGraph.Maps
+public import Mathlib.Combinatorics.SimpleGraph.Clique
 /-!
 
 # Planar graphs
+
+## Table of contents
+
+- A. Maps
+- B. Inequalities
+- C. Examples
 
 -/
 @[expose] public section
@@ -38,13 +44,20 @@ def IsPlanar (G : SimpleGraph V) : Prop :=
 namespace IsPlanar
 
 --~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
+/- ## A. Maps -/
 section Maps
+
+lemma hom {G : SimpleGraph V} {H : SimpleGraph W} (f : H →g G) (hf : Function.Injective f)
+    (hG : G.IsPlanar) : H.IsPlanar := by
+  obtain ⟨g, hg⟩ := hG
+  use ⟨g ∘ f, g.injective.comp hf⟩
+  exact fun v v' w w' h₁ h₂ h₃ h₄ h₅ h₆ ↦ hg (f v) (f v') (f w) (f w') (f.map_adj h₁) (f.map_adj h₂)
+    (fun h ↦ h₃ <| hf h) (fun h ↦ h₄ <| hf h) (fun h ↦ h₅ <| hf h) (fun h ↦ h₆ <| hf h)
 
 /-- A graph that embeds into a planar graph is planar. -/
 lemma embedding {G : SimpleGraph V} {H : SimpleGraph W} (f : H ↪g G) (hG : G.IsPlanar) :
-    H.IsPlanar := by
-  obtain ⟨g, hg⟩ := hG
-  exact ⟨f.toEmbedding.trans g, fun v v' w w' ↦ by simpa using hg (f v) (f v') (f w) (f w')⟩
+    H.IsPlanar :=
+  hG.hom f.toHom f.injective
 
 /-- Planarity is preserved under graph isomorphism. -/
 lemma iso {G : SimpleGraph V} {H : SimpleGraph W} (f : H ≃g G) (hG : G.IsPlanar) : H.IsPlanar :=
@@ -55,34 +68,54 @@ lemma induce {G : SimpleGraph V} (s : Set V) (hG : G.IsPlanar) : (G.induce s).Is
   hG.embedding <| .induce s
 
 /-- A subgraph of a planar simple graph is planar. -/
-lemma mono {G H : SimpleGraph V} (h : H ≤ G) (hG : G.IsPlanar) : H.IsPlanar := by
-  obtain ⟨g, hg⟩ := hG
-  exact ⟨g, fun v v' w w' hv hw ↦ hg v v' w w' (h hv) (h hw)⟩
+lemma mono {G H : SimpleGraph V} (h : H ≤ G) (hG : G.IsPlanar) : H.IsPlanar :=
+  hG.hom (.ofLE h) fun _ _ h' ↦ h'
 
 end Maps
 --~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
 
 --~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
-section CompleteGraph
+/- ## B. Inequalities -/
+section Inequalities
+
+lemma ncard_edgeSet_le {G : SimpleGraph V} (h : 3 ≤ G.support.ncard) (hG : G.IsPlanar) :
+    G.edgeSet.ncard ≤ 3 * G.support.ncard - 6 :=
+  sorry
+
+lemma ncard_edgeSet_le_of_triangleFree
+    {G : SimpleGraph V} (h : 3 ≤ G.support.ncard) (h' : G.CliqueFree 3) (hG : G.IsPlanar) :
+    G.edgeSet.ncard ≤ 2 * G.support.ncard - 4 :=
+  sorry
+
+end Inequalities
+--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
+
+--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
+/- ## C. Examples -/
+section Examples
+
+/-- The empty graph is planar provided an embedding of `V` into the plane exists. -/
+lemma emptyGraph (f : V ↪ ℝ²) : (emptyGraph V).IsPlanar := ⟨f, by simp⟩
 
 /-- K₄ is planar. -/
 lemma completeGraph_four : (completeGraph (Fin 4)).IsPlanar := sorry
 
 /-- K₅ is non-planar. -/
-lemma not_completeGraph_five : ¬(completeGraph (Fin 5)).IsPlanar := sorry
+lemma not_completeGraph_five : ¬(completeGraph (Fin 5)).IsPlanar := by
+  refine fun h5 ↦ not_lt_of_ge (h5.ncard_edgeSet_le ?_) ?_
+  · simp [support_top_of_nontrivial]
+  · calc
+      _ < 10 := by simp [support_top_of_nontrivial]
+      _ = Nat.choose 5 2 := by decide
+      _ = (completeGraph (Fin 5)).edgeSet.ncard := by
+        simpa [← Set.fintypeCard_eq_ncard] using Sym2.card_diagSet_compl (α := Fin 5).symm
 
-/-- Kₙ is planar iff `n < 5` -/
+/-- Kₙ is planar iff `n < 5`. -/
 lemma completeGraph_iff_lt_five {n : ℕ} : (completeGraph (Fin n)).IsPlanar ↔ n < 5 := by
   constructor <;> intro h
   · by_contra! hn
     exact not_completeGraph_five <| h.embedding <| .completeGraph <| Fin.castLEEmb hn
   · exact completeGraph_four.embedding <| .completeGraph <| Fin.castLEEmb (Nat.le_of_succ_le_succ h)
-
-end CompleteGraph
---~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
-
---~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
-section CompleteBipartiteGraph
 
 /-- Embeddings of types induce embeddings of complete bipartite graphs on those types. -/
 protected def _root_.SimpleGraph.Embedding.completeBipartiteGraph
@@ -124,7 +157,7 @@ lemma completeBipartiteGraph_iff_lt_three (m n : ℕ) :
       refine (completeBipartiteGraph_two m).embedding ?_
       exact .completeBipartiteGraph (Fin.castLEEmb <| Nat.le_of_succ_le_succ hn) (.refl _)
 
-end CompleteBipartiteGraph
+end Examples
 --~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
 
 end IsPlanar
