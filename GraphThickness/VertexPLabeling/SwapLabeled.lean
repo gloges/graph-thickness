33767b51f3d4db22e86b22e8e159053d071f0d23
Gregory Loges <gerg1995@gmail.com>
2026-09-09 15:11:18 -0400

    feat: add `swapOrbit` (#16)
    * add orbitRel lemma
    
    * var reorganize
    
    * add labeling lift
    
    * add swapOrbit

--- GraphThickness/VertexPLabeling/SwapLabeled.lean
+++ GraphThickness/VertexPLabeling/SwapLabeled.lean
@@ -25,25 +25,27 @@ namespace VertexPLabeling
 
 open Equiv
 
+variable (l : V →ᵥ. K)
+
 /-- The permutation of `V ⊕ V` which is the identity on unlabeled vertices
   and `Sum.swap` on labeled vertices. -/
-def swapLabeled (l : V →ᵥ. K) : Perm (V ⊕ V) :=
-  Function.Involutive.toPerm
-    (fun x ↦ if (l ⊕g l) x = none then x else x.swap)
+def swapLabeled : Perm (V ⊕ V) :=
+  Function.Involutive.toPerm (fun x ↦ if (l ⊕g l) x = none then x else x.swap)
     fun x ↦ by rcases eq_or_ne ((l ⊕g l) x) none <;> simp_all
 
-variable (l : V →ᵥ. K) (v : V) (x y : V ⊕ V)
+@[simp]
+lemma swapLabeled_inl (v : V) : l.swapLabeled (.inl v) = if l v = none then .inl v else .inr v :=
+  rfl
 
 @[simp]
-lemma swapLabeled_inl : l.swapLabeled (.inl v) = if l v = none then .inl v else .inr v := rfl
-
-@[simp]
-lemma swapLabeled_inr : l.swapLabeled (.inr v) = if l v = none then .inr v else .inl v := rfl
+lemma swapLabeled_inr (v : V) : l.swapLabeled (.inr v) = if l v = none then .inr v else .inl v :=
+  rfl
 
 lemma swapLabeled_involutive : Function.Involutive l.swapLabeled := .toPerm_involutive _
 
 @[simp]
-lemma swapLabeled_swapLabeled : l.swapLabeled (l.swapLabeled x) = x := l.swapLabeled_involutive x
+lemma swapLabeled_swapLabeled (x : V ⊕ V) : l.swapLabeled (l.swapLabeled x) = x :=
+  l.swapLabeled_involutive x
 
 /-- `l.swapLabeled` acts as the identity on unlabeled vertices. -/
 lemma unlabeled_domRestrict_swapLabeled :
@@ -78,7 +80,11 @@ end Pow
 --~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
 section Orbits
 
-variable {l v x y}
+variable {l} {v : V} {x y : V ⊕ V}
+
+@[aesop safe forward]
+lemma exists_eq_orbit (a : Orbit l.swapLabeled) : ∃ x : V ⊕ V, a = ⟦x, l.swapLabeled⟧ :=
+  ⟨a.out, (Orbit.orbit_out_eq a).symm⟩
 
 @[simp]
 lemma orbit_eq_of_labeled (h : l v ≠ none) : ⟦.inl v, l.swapLabeled⟧ = ⟦.inr v, l.swapLabeled⟧ := by
@@ -100,6 +106,43 @@ lemma orbit_eq_iff_eq_or_eq_swapLabeled :
     ⟦x, l.swapLabeled⟧ = ⟦y, l.swapLabeled⟧ ↔ x = y ∨ x = l.swapLabeled y := by aesop
 
 end Orbits
+--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
+
+--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
+section Lift
+
+/-- Lift the labeling `l ⊕g l` of `V ⊕ V` to a labeling of the orbits under `l.swapLabeled`. -/
+protected def lift : Orbit l.swapLabeled →ᵥ. K := Quotient.lift (l ⊕g l) fun _ _ _ ↦ by aesop
+
+@[simp]
+lemma lift_apply (x : V ⊕ V) : l.lift ⟦x, l.swapLabeled⟧ = (l ⊕g l) x := rfl
+
+end Lift
+--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
+
+--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
+section SwapOrbit
+
+/-- `Sum.swap` mapped to the orbits of `V ⊕ V` under `l.swapLabeled`.
+
+  `l.swapOrbit` sends `⟦x, l.swapLabeled⟧` to `⟦x.swap, l.swapLabeled⟧`. -/
+def swapOrbit : Perm (Orbit l.swapLabeled) :=
+  Function.Involutive.toPerm (Quotient.map Sum.swap <| by aesop) fun _ ↦ by aesop
+
+@[simp]
+lemma swapOrbit_apply (x : V ⊕ V) : l.swapOrbit ⟦x, l.swapLabeled⟧ = ⟦x.swap, l.swapLabeled⟧ := rfl
+
+lemma swapOrbit_involutive : Function.Involutive l.swapOrbit := .toPerm_involutive _
+
+@[simp]
+lemma swapOrbit_swapOrbit (a : Orbit l.swapLabeled) : l.swapOrbit (l.swapOrbit a) = a :=
+  l.swapOrbit_involutive _
+
+@[simp]
+lemma lift_swapOrbit_eq_lift (a : Orbit l.swapLabeled) : l.lift (l.swapOrbit a) = l.lift a := by
+  aesop
+
+end SwapOrbit
 --~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~==~~--~~
 
 end VertexPLabeling
